618b315813856df8bf2d319765b2ce28ea6aeae4
MichaelThoresen <mimothor@hotmail.com>
2018-09-03 13:40:52 +0200

    Implemented ombyttinger, has bugs that need stomping

--- Oblig 1/src/com/company/Main.java
+++ Oblig 1/src/com/company/Main.java
@@ -3,20 +3,34 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int[] a = new int[] {3,7,4,8,2};
+        int[] a = new int[] {8,7,4,3,2};
         System.out.println(maks(a));
+        System.out.println(ombyttinger(a));
     }
 
     public static int maks(int[] a){
-        for(int i = 0; i<a.length-1; i++) if(a[i]> a[i+1]){
-            bytt(a, i);
+        for(int i = 0; i<a.length-1; i++){
+            if(a[i]> a[i+1]){
+                bytt(a, i, i+1);
+            }
         }
         return a[a.length-1];
     }
 
-    public static void bytt(int[] a, int i) {
+    public static void bytt(int[] a, int i, int j) {
         int tmp = a[i];
-        a[i] = a[i+1];
-        a[i+1] = tmp;
+        a[i] = a[j];
+        a[j] = tmp;
+    }
+
+    public static int ombyttinger(int[] a){
+        int antallOmbyttinger = 0;
+        for(int i=0; i<a.length-1; i++) {
+            if(a[i]>a[i+1]) {
+                bytt(a, i, i+1);
+                antallOmbyttinger++;
+            }
+        }
+        return antallOmbyttinger;
     }
 }
