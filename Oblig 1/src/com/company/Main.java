36858dfc6d60dd092cb7977ad0d4dc20bc8442f7
Ole-Martin <ole-ma@outlook.com>
2018-09-04 15:39:10 +0200

    Fixed copying arrays in test cases

--- Oblig 1/src/com/company/Main.java
+++ Oblig 1/src/com/company/Main.java
@@ -7,9 +7,9 @@ public class Main {
 
     public static void main(String[] args) {
         int[] a = new int[] {6,2,3,4,5,1,7,8,9};
-        int[] b = a;
+        int[] b = a.clone();
         int[] c = new int[] {3,3,4,5,5,6,7,7,7,8};
-        int[] d = c;
+        int[] d = c.clone();
         System.out.println(maks(a));
         System.out.println(ombyttinger(b));
         System.out.println(antallUlikeSortert(c));
